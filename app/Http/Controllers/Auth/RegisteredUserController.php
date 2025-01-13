96df4aeccdf5495114c22e255f25cd3e1c6ebdbd
Szilveszter Ábrám <szilveszter@webgurus.io>
2025-01-13 09:24:32 +0200

    Added user role assignment upon registering

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Enums\RoleEnum;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
@@ -45,6 +46,8 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
+
+        $user->assignRole(RoleEnum::USER);
 
         return redirect(route('welcome', absolute: false));
     }
